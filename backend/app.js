abc79bf2e9a5064ef107e88c945fc94f1b4a2f29
Дмитрий Колесников <dmitrijkolesnikov@MacBook-Air-Dmitrij.local>
2024-12-24 23:21:22 +0300

    add env

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -11,12 +13,8 @@ app.use(express.json());
 
 app.use("/", routes);
 
-mongoose
-  .connect(
-    "mongodb+srv://user:qwe123@cluster.gfck5.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster"
-  )
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
-    });
+mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
   });
+});
